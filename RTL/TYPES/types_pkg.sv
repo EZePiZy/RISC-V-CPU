e7105243b94395ab190414262440a9f948efa369
Diegovano <36131808+Diegovano@users.noreply.github.com>
2022-12-11 17:43:28 +0000

    feat: rename jumpSaveNext to clarify usage, remove vbuddy.cfg from repo as every contributor will have different contents, run.sh no longer writes to cfg as every contributor required different ports.

--- RTL/TYPES/types_pkg.sv
+++ RTL/TYPES/types_pkg.sv
@@ -11,7 +11,7 @@ package types_pkg;
   typedef logic [DATA_WIDTH-1:0] DATA_BUS;
 
   // possible formats for immediates
-  typedef enum bit[IMM_WIDTH-1:0] {I = 3'b000, UI = 3'b001, S = 3'b010, B = 3'b011, J = 3'b100} instr_format;
+  typedef enum bit[IMM_WIDTH-1:0] {Imm = 3'b000, UpperImm = 3'b001, Store = 3'b010, Branch = 3'b011, Jump = 3'b100} instr_format;
 
   // mapping opcode types to binary values
   typedef enum bit[OPCODE_WIDTH-1:0] {R = 7'b0110011, I1 = 7'b0000011, I2 = 7'b0010011, S = 7'b0100011, B = 7'b1100011, U1 = 7'b0010111, U2 = 7'b0110111, I3 = 7'b1100111, J = 7'b1101111} opcode;
