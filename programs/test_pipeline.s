06b2337901215ff829e8d13e15deb415e4061a7c
Ezra Reich <64613180+EZePiZy@users.noreply.github.com>
2022-12-13 22:05:44 +0000

    fix: edit pipeline test code to use pseudoinstruction

--- programs/test_pipeline.s
+++ programs/test_pipeline.s
@@ -1,91 +1,100 @@
 # Simplest F1 program
 start:
+    nop
+    nop
+    nop
+    nop
+    nop
     addi a1, zero, 8
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
+    nop
+    nop
+    nop
+    nop
+    nop
     addi a0, zero, 0
+    nop
+    nop
+    nop
+    nop
+    nop
 countdown:
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
     slli a0, a0, 1
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP  
-    addi a0, a0, 1 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
+    nop
+    nop
+    nop
+    nop
+    nop
+    addi a0, a0, 1
+    nop
+    nop
+    nop
+    nop
+    nop 
     addi a1, a1, -1
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
+    nop
+    nop
+    nop
+    nop
+    nop
     bne a1, zero, countdown 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
+    nop
+    nop
+    nop
+    nop
+    nop
     addi a2, a2, 74
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
+    nop
+    nop
+    nop
+    nop
+    nop
     jal sp, delay 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
+    nop
+    nop
+    nop
+    nop
+    nop
     addi a0, zero, 0 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
+    nop
+    nop
+    nop
+    nop
+    nop
     jal zero, start
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
+    nop
+    nop
+    nop
+    nop
+    nop
 delay:
     addi t0, t0, 10
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
+    nop
+    nop
+    nop
+    nop
+    nop
 loop:                   # Loops 10 times
     addi t0, t0, -1
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
+    nop
+    nop
+    nop
+    nop
+    nop
     addi zero, zero, 0  # NOP
+    nop
+    nop
+    nop
+    nop
+    nop
     bne t0, zero, loop
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
-    jalr zero, sp, 0    # return 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP
-    addi a0, zero, a0 // NOP 
-    addi a0, zero, a0 // NOP 
+    nop
+    nop
+    nop
+    nop
+    nop
+    jalr zero, sp, 0    # return
+    nop
+    nop
+    nop
+    nop
+    nop
