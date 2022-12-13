7ac4fcc5739f673365ecc54f7cf7f3b6ab4d6399
omalley242 <omalley242@gmail.com>
2022-12-13 20:51:35 +0000

    feat: finishing pipelining

--- RTL/EXECUTE_MEMORY_REGISTER/EXECUTE_MEMORY_REGISTER.sv
+++ RTL/EXECUTE_MEMORY_REGISTER/EXECUTE_MEMORY_REGISTER.sv
@@ -3,20 +3,37 @@ module FETCH_DECODE_REGISTER
 import types_pkg::*; 
 
 (
-    input   logic       clk,
-    input   DATA_BUS    instructionF_i,
-    output  DATA_BUS    instructionD_o,
-    input   DATA_BUS    PCF_i,
-    output  DATA_BUS    PCD_o,
-    input   DATA_BUS    PCPlus4F_i,
-    output  DATA_BUS    PCPlus4D_o
+    input    logic        clk,
+    input    DATA_BUS        PCPlus4E_i,
+    output   DATA_BUS        PCPlus4M_o,
+    input    DATA_BUS        ALU_outE_i,
+    output   DATA_BUS        ALU_outM_o,
+    input    ADDRESS_WIDTH   RdE_i,
+    output   ADDRESS_WIDTH   RdM_o,
+    input    logic           RegWriteE_i, 
+    output   logic           RegWriteM_o,
+    input    logic           MemWriteE_i,
+    output   logic           MemWriteM_o,
+    input    logic           ResultSrcE_i,
+    output   logic           ResultSrcM_o,
+    input    logic           StoreNextPCE_i,
+    output   logic           StoreNextPCM_o,
+    input    logic           MemExtendE_i,
+    output   logic           MemExtendM_o,
+    input    byte_format     ByteSelectE_i,
+    output   byte_format     ByteSelectM_o
 );
 
 always_ff @(negedge clk) begin
-    instructionD_o <= instructionF_i;
-    PCD_o <= PCF_i;
-    PCPlus4D_o <= PCPlus4F_i;
+    PCPlus4M_o <= PCPlus4E_i;
+    ALU_outM_o <= ALU_outE_i;
+    RdM_o <= RdE_i;
+    RegWriteM_o <= RegWriteE_i;
+    MemWriteM_o <= MemWriteE_i;
+    ResultSrcM_o <= ResultSrcE_i;
+    StoreNextPCM_o <= StoreNextPCE_i;
+    MemExtendM_o <= MemExtendE_i;
+    ByteSelectM_o <= ByteSelectE_i;
 end
 
 endmodule 
-
--- RTL/MEMORY_WRITEBACK_REGISTER/MEMORY_WRITEBACK_REGISTER.sv
+++ RTL/MEMORY_WRITEBACK_REGISTER/MEMORY_WRITEBACK_REGISTER.sv
@@ -3,19 +3,31 @@ module FETCH_DECODE_REGISTER
 import types_pkg::*; 
 
 (
-    input   logic       clk,
-    input   DATA_BUS    instructionF_i,
-    output  DATA_BUS    instructionD_o,
-    input   DATA_BUS    PCF_i,
-    output  DATA_BUS    PCD_o,
-    input   DATA_BUS    PCPlus4F_i,
-    output  DATA_BUS    PCPlus4D_o
+    input   logic           clk,
+    input   DATA_BUS        ReadDataM_i,
+    output  DATA_BUS        ReadDataW_o,
+    input   DATA_BUS        PCPlus4M_i,
+    output  DATA_BUS        PCPlus4W_o,
+    input   DATA_BUS        ALU_outM_i,
+    output  DATA_BUS        ALU_outW_o,
+    input   logic           RegWriteM_i,
+    output  logic           RegWriteW_o,
+    input   logic           ResultSrcM_i,
+    output  logic           ResultSrcW_o,
+    input   logic           StoreNextPCM_i,
+    output  logic           StoreNextPCW_o,
+    input   ADDRESS_WIDTH   RdM_i,
+    output  ADDRESS_WIDTH   RdW_o
 );
 
 always_ff @(negedge clk) begin
-    instructionD_o <= instructionF_i;
-    PCD_o <= PCF_i;
-    PCPlus4D_o <= PCPlus4F_i;
+    ReadDataW_o <= ReadDataM_i;
+    PCPlus4W_o <= PCPlus4M_i;
+    ALU_outW_o <= ALU_outM_i;
+    RegWriteW_o <= RegWriteM_i;
+    ResultSrcW_o <= ResultSrcM_i;
+    StoreNextPCW_o <= StoreNextPCM_i;
+    RdW_o <= RdM_i;
 end
 
 endmodule 
